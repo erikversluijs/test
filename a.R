cf44e41dbcdb01b2fd982e19e2f444280bf5272d
erikversluijs <erikversluijs@posteo.net>
2023-11-17 09:39:30 +0100

    intial commit n3

---
+++ a.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+# One Job, One commit
+library(usethis)
+use_git_config(
+  user.name = "erikversluijs",
+  user.email = "erikversluijs@posteo.net"
+)
+
+usethis::create_github_token()
+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
